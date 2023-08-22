Fixed gradient adjoint test -- no reflect bc's

operators.py
@@ -69,76 +69,24 @@ def fspecial(shape=(3,3),sigma=0.5, ret_torch=False):
         h = h.to(torch.float)
     return h
 
-def fspecial_1d(kernel_size=3, sigma=0.5, ret_torch=False):
-    """
-    1D gaussian mask - equivalent to MATLAB's fspecial('gaussian', [shape], [sigma])
-    """
-    x = np.arange(-(kernel_size - 1) / 2, (kernel_size + 1) / 2)
-    h = np.exp(-x**2 / (2 * sigma**2))
-    h[h < np.finfo(h.dtype).eps * h.max()] = 0
-    sumh = h.sum()
-    if sumh != 0:
-        h /= sumh
-    if ret_torch:
-        h = torch.from_numpy(h)
-        h = h.to(torch.float)
-    return h
 
-def blur_operator_torch_1d(org, shape=9, sigma=4.0):
-    if type(org) != torch.Tensor:
-        org = torch.from_numpy(org)
+# def blur_operator_torch(org, shape=(9,9), sigma=4.0, transpose=False):
+#     if type(org) != torch.Tensor:
+#         org = torch.from_numpy(org)
 
-    if len(org.size()) > 1:
-        org = org.view(-1)
-
-    psf = fspecial_1d(shape,sigma)
-    padding = nn.ReflectionPad1d(shape // 2)
-    org = org.unsqueeze(0)
-    org_pad = padding(org)
-    org_pad = org_pad.to(torch.float)
-    blurred = F.conv1d(org_pad, psf.unsqueeze(0).unsqueeze(0))
-    blurred = blurred.squeeze()
-
-    return blurred 
-
-def blur_adjoint_torch_1d(org, shape=9, sigma=4):
-    """
-    The transpose will come just from completely flipping the convolution kernel, and then performing the convolution operation.
-    """
-    if type(org) != torch.Tensor:
-        org = torch.from_numpy(org)
-
-    if len(org.size()) > 1:
-        org = org.view(-1)
-
-    psf = fspecial_1d(shape, sigma)
-    psf = torch.flip(psf, [0])
-    padding = nn.ReflectionPad1d(shape // 2)
-    org = org.unsqueeze(0)
-    org_pad = padding(org)
-    org_pad = org_pad.to(torch.float)
-    adjoint_blurred = F.conv1d(org_pad, psf.unsqueeze(0).unsqueeze(0))
-    adjoint_blurred = adjoint_blurred.squeeze()
-
-    return adjoint_blurred
-
-def blur_operator_torch(org, shape=(9,9), sigma=4.0, transpose=False):
-    if type(org) != torch.Tensor:
-        org = torch.from_numpy(org)
-
-    if len(org.size()) == 1:
-        m = int(np.sqrt(org.size(0)))
-        org = torch.reshape(org, (m,m))
+#     if len(org.size()) == 1:
+#         m = int(np.sqrt(org.size(0)))
+#         org = torch.reshape(org, (m,m))
     
-    org = org.unsqueeze(0)
-    blurrer = GaussianBlur(shape, sigma) #https://pytorch.org/vision/main/generated/torchvision.transforms.GaussianBlur.html
-    blurred = blurrer(org)
-    blurred = blurred.squeeze()
-    if transpose:
-        blurred = blurred.T
-    blurred = torch.flatten(blurred)
+#     org = org.unsqueeze(0)
+#     blurrer = GaussianBlur(shape, sigma) #https://pytorch.org/vision/main/generated/torchvision.transforms.GaussianBlur.html
+#     blurred = blurrer(org)
+#     blurred = blurred.squeeze()
+#     if transpose:
+#         blurred = blurred.T
+#     blurred = torch.flatten(blurred)
 
-    return blurred
+#     return blurred
 # def blur_operator_torch(org, shape=(9,9), sigma=4.0):
 #     if type(org) != torch.Tensor:
 #         org = torch.from_numpy(org)
@@ -150,37 +98,77 @@ def blur_operator_torch(org, shape=(9,9), sigma=4.0, transpose=False):
 #     psf = fspecial(shape,sigma,True)
 #     padding = nn.ReflectionPad2d(4)
 #     org = org.unsqueeze(0).unsqueeze(0)
-#     org_pad = padding(org)
-#     org_pad = org_pad.to(torch.float)
-#     blurred = F.conv2d(org_pad, psf.unsqueeze(0).unsqueeze(0))
+#     # org_pad = padding(org)
+#     # org_pad = org_pad.to(torch.float)
+#     blurred = F.conv2d(org, psf.unsqueeze(0).unsqueeze(0), padding=padding)
 #     blurred = blurred.squeeze()
 #     blurred = torch.flatten(blurred)
 
 #     return blurred 
-
-def blur_adjoint_torch(org, shape=(9,9), sigma=4):
-    """
-    The transpose will come just from completely flipping the convolution kernel, and then performing the convolution operation.
-    """
+def blur_operator_torch(org, reshape=True, shape=(9, 9), sigma=4, mode="reflect"):
     if type(org) != torch.Tensor:
         org = torch.from_numpy(org)
 
     if len(org.size()) == 1:
         m = int(np.sqrt(org.size(0)))
         org = torch.reshape(org, (m,m))
+    #TODO: I have this code running now, and it works great, but it doesn't take into accound the reflective bc's.
+    # Why is this the case?
+    
+    psf = fspecial(shape, sigma, ret_torch=True)
+    pad_size = shape[0] // 2
+    # org_padded = reflect_padding(org.unsqueeze(0).unsqueeze(0), pad_size)
+    blurred = F.conv2d(org.unsqueeze(0).unsqueeze(0), psf.unsqueeze(0).unsqueeze(0),padding="same")
+    blurred = blurred.squeeze()
+    
+    if reshape:
+        blurred = blurred.flatten(start_dim=0, end_dim=1)
+    
+    return blurred
 
-    psf = fspecial(shape, sigma, True)
-    # psf = torch.roll(torch.flip(psf, [0,1]),shifts=(1,1))
-    psf = torch.flip(psf, [0,1])
-    padding = nn.ReflectionPad2d(4)
-    org = org.unsqueeze(0).unsqueeze(0)
-    org_pad = padding(org)
-    org_pad = org_pad.to(torch.float)
-    adjoint_blurred = F.conv2d(org_pad, psf.unsqueeze(0).unsqueeze(0))
+def blur_adjoint_torch(org, reshape=True, shape=(9, 9), sigma=4, mode="reflect"):
+    if type(org) != torch.Tensor:
+        org = torch.from_numpy(org)
+
+    if len(org.size()) == 1:
+        m = int(np.sqrt(org.size(0)))
+        org = torch.reshape(org, (m,m))
+    
+    psf = fspecial(shape, sigma, ret_torch=True)
+    psf = torch.flip(psf, [0, 1])
+    pad_size = shape[0] // 2
+    # org_padded = reflect_padding(org.unsqueeze(0).unsqueeze(0), pad_size)
+    adjoint_blurred = F.conv2d(org.unsqueeze(0).unsqueeze(0), psf.unsqueeze(0).unsqueeze(0),padding="same")
     adjoint_blurred = adjoint_blurred.squeeze()
-    adjoint_blurred = torch.flatten(adjoint_blurred)
-
+    
+    if reshape:
+        adjoint_blurred = adjoint_blurred.flatten(start_dim=0, end_dim=1)
+    
     return adjoint_blurred
+
+# def blur_adjoint_torch(org, shape=(9,9), sigma=4):
+#     """
+#     The transpose will come just from completely flipping the convolution kernel, and then performing the convolution operation.
+#     """
+#     if type(org) != torch.Tensor:
+#         org = torch.from_numpy(org)
+
+#     if len(org.size()) == 1:
+#         m = int(np.sqrt(org.size(0)))
+#         org = torch.reshape(org, (m,m))
+
+#     psf = fspecial(shape, sigma, True)
+#     # psf = torch.roll(torch.flip(psf, [0,1]),shifts=(1,1))
+#     psf = torch.flip(psf, [0,1])
+#     padding = nn.ReflectionPad2d(4)
+#     org = org.unsqueeze(0).unsqueeze(0)
+#     # org_pad = padding(org)
+#     # org_pad = org_pad.to(torch.float)
+#     adjoint_blurred = F.conv2d(org, psf.unsqueeze(0).unsqueeze(0), padding=padding)
+#     adjoint_blurred = adjoint_blurred.squeeze()
+#     adjoint_blurred = torch.flatten(adjoint_blurred)
+
+#     return adjoint_blurred
 
 def blur_adjoint(org, reshape=True, shape=(9,9), sigma=4, mode="reflect"):
     if reshape: