9da0b4a945d827dd9cb5bd137f50ea23b4563541
Alexander Johnson <abjohnson117@gmail.com>
2023-09-07 14:34:50 -0700

    Correct evals for blur

--- operators.py
+++ operators.py
@@ -6,6 +6,7 @@ from scipy.fftpack import dct
 import torch
 from torch import nn
 import torch.nn.functional as F
+import torch_dct as dct_t
 from torchvision.transforms import GaussianBlur
 from pytorch_wavelets import DWT1DForward, DWT1DInverse
 
@@ -73,63 +74,25 @@ def fspecial(shape=(3,3),sigma=0.5, ret_torch=False):
     return h
 
 
-# def blur_operator_torch(org, shape=(9,9), sigma=4.0, transpose=False):
-#     if type(org) != torch.Tensor:
-#         org = torch.from_numpy(org)
-
-#     if len(org.size()) == 1:
-#         m = int(np.sqrt(org.size(0)))
-#         org = torch.reshape(org, (m,m))
-    
-#     org = org.unsqueeze(0)
-#     blurrer = GaussianBlur(shape, sigma) #https://pytorch.org/vision/main/generated/torchvision.transforms.GaussianBlur.html
-#     blurred = blurrer(org)
-#     blurred = blurred.squeeze()
-#     if transpose:
-#         blurred = blurred.T
-#     blurred = torch.flatten(blurred)
-
-#     return blurred
-# def blur_operator_torch(org, shape=(9,9), sigma=4.0):
-#     if type(org) != torch.Tensor:
-#         org = torch.from_numpy(org)
-
-#     if len(org.size()) == 1:
-#         m = int(np.sqrt(org.size(0)))
-#         org = torch.reshape(org, (m,m))
-
-#     psf = fspecial(shape,sigma,True)
-#     padding = nn.ReflectionPad2d(4)
-#     org = org.unsqueeze(0).unsqueeze(0)
-#     # org_pad = padding(org)
-#     # org_pad = org_pad.to(torch.float)
-#     blurred = F.conv2d(org, psf.unsqueeze(0).unsqueeze(0), padding=padding)
-#     blurred = blurred.squeeze()
-#     blurred = torch.flatten(blurred)
-
-#     return blurred 
-def blur_operator_torch(org, reshape=True, shape=(9, 9), sigma=8, mode="reflect"):
+def blur_operator_torch(org, shape=(9,9), sigma=4.0, reshape=True):
     if type(org) != torch.Tensor:
         org = torch.from_numpy(org)
 
     if len(org.size()) == 1:
         m = int(np.sqrt(org.size(0)))
         org = torch.reshape(org, (m,m))
-    #TODO: I have this code running now, and it works great, but it doesn't take into account the reflective bc's.
-    # Why is this the case?
     
-    psf = fspecial(shape, sigma, ret_torch=True)
-    # pad_size = shape[0] // 2
-    # org_padded = reflect_padding(org.unsqueeze(0).unsqueeze(0), pad_size)
-    blurred = F.conv2d(org.unsqueeze(0).unsqueeze(0), psf.unsqueeze(0).unsqueeze(0),padding="same")
+    org = org.unsqueeze(0)
+    blurrer = GaussianBlur(shape, sigma) #https://pytorch.org/vision/main/generated/torchvision.transforms.GaussianBlur.html
+    blurred = blurrer(org)
     blurred = blurred.squeeze()
-    
+
     if reshape:
         blurred = blurred.flatten(start_dim=0, end_dim=1)
-    
+
     return blurred
 
-def blur_adjoint_torch(org, reshape=True, shape=(9, 9), sigma=8, mode="reflect"):
+def blur_adjoint_torch(org, shape=(9,9), sigma=4.0, reshape=True):
     if type(org) != torch.Tensor:
         org = torch.from_numpy(org)
 
@@ -137,19 +100,62 @@ def blur_adjoint_torch(org, reshape=True, shape=(9, 9), sigma=8, mode="reflect")
         m = int(np.sqrt(org.size(0)))
         org = torch.reshape(org, (m,m))
 
-    #https://dsp.stackexchange.com/questions/2340/meaning-of-the-transpose-of-convolution
-    
     psf = fspecial(shape, sigma, ret_torch=True)
-    psf = torch.flip(psf, [0, 1])
-    pad_size = shape[0] // 2
-    # org_padded = reflect_padding(org.unsqueeze(0).unsqueeze(0), pad_size)
-    adjoint_blurred = F.conv2d(org.unsqueeze(0).unsqueeze(0), psf.unsqueeze(0).unsqueeze(0),padding="same")
-    adjoint_blurred = adjoint_blurred.squeeze()
+    psf = torch.flip(psf, [0,1])
+    padding = nn.ReflectionPad2d(4)
+    org = org.unsqueeze(0).unsqueeze(0)
+    org_pad = padding(org)
+    org_pad = org_pad.to(torch.float)
+    adj_blurred = F.conv2d(org_pad, psf.unsqueeze(0).unsqueeze(0))
+    adj_blurred = adj_blurred.squeeze()
     
     if reshape:
-        adjoint_blurred = adjoint_blurred.flatten(start_dim=0, end_dim=1)
+        adj_blurred = adj_blurred.flatten(start_dim=0, end_dim=1)
     
-    return adjoint_blurred
+    return adj_blurred
+
+# def blur_operator_torch(org, reshape=True, shape=(9, 9), sigma=8, mode="reflect"):
+#     if type(org) != torch.Tensor:
+#         org = torch.from_numpy(org)
+
+#     if len(org.size()) == 1:
+#         m = int(np.sqrt(org.size(0)))
+#         org = torch.reshape(org, (m,m))
+#     #TODO: I have this code running now, and it works great, but it doesn't take into account the reflective bc's.
+#     # Why is this the case?
+    
+#     psf = fspecial(shape, sigma, ret_torch=True)
+#     # pad_size = shape[0] // 2
+#     # org_padded = reflect_padding(org.unsqueeze(0).unsqueeze(0), pad_size)
+#     blurred = F.conv2d(org.unsqueeze(0).unsqueeze(0), psf.unsqueeze(0).unsqueeze(0),padding="same")
+#     blurred = blurred.squeeze()
+    
+#     if reshape:
+#         blurred = blurred.flatten(start_dim=0, end_dim=1)
+    
+#     return blurred
+
+# def blur_adjoint_torch(org, reshape=True, shape=(9, 9), sigma=8, mode="reflect"):
+#     if type(org) != torch.Tensor:
+#         org = torch.from_numpy(org)
+
+#     if len(org.size()) == 1:
+#         m = int(np.sqrt(org.size(0)))
+#         org = torch.reshape(org, (m,m))
+
+#     #https://dsp.stackexchange.com/questions/2340/meaning-of-the-transpose-of-convolution
+    
+#     psf = fspecial(shape, sigma, ret_torch=True)
+#     psf = torch.flip(psf, [0, 1])
+#     pad_size = shape[0] // 2
+#     org_padded = reflect_padding(org.unsqueeze(0).unsqueeze(0), pad_size)
+#     adjoint_blurred = F.conv2d(org.unsqueeze(0).unsqueeze(0), psf.unsqueeze(0).unsqueeze(0),padding="same")
+#     adjoint_blurred = adjoint_blurred.squeeze()
+    
+#     if reshape:
+#         adjoint_blurred = adjoint_blurred.flatten(start_dim=0, end_dim=1)
+    
+#     return adjoint_blurred
 
 # def blur_adjoint_torch(org, shape=(9,9), sigma=4):
 #     """
@@ -189,14 +195,15 @@ def blur_adjoint(org, reshape=True, shape=(9,9), sigma=4, mode="reflect"):
 
     return adjoint_blurred
 
-def dctshift(psf, center=(4,4)):
+def dctshift(psf):
     """Taken from Deblurring Images to compute first column of A  matrix"""
+    center = (psf.shape[0] // 2, psf.shape[1] // 2)
     m, n = psf.shape[0], psf.shape[1]
     i = center[0]
     j = center[1]
-    k = min([i,m-i,j,n-j])
+    k = min([i-1,m-i,j-1,n-j])
 
-    PP = psf[i-(k):i+(k+1),j-(k):j+(k+1)]
+    PP = psf[i-(k+1):i+(k),j-(k+1):j+(k)]
     Z1 = np.diag(np.ones(k+1), k)
     Z2 = np.diag(np.ones(k), k+1)
 
@@ -206,15 +213,44 @@ def dctshift(psf, center=(4,4)):
 
     return Ps
 
-def evals_blur(psf):
-    """Calculates eigenvalues according to equation in Deblurring Images"""
-    a1 = dctshift(psf)
-    
-    e1 = np.zeros_like(a1)
-    e1[0,0] = 1
+def dctshift_torch(psf):
+    """Taken from Deblurring Images to compute first column of blur operator matrix A"""
+    center = (psf.size(0) // 2, psf.size(1) // 2)
+    m, n = psf.size(0), psf.size(1)
+    i, j = center[0], center[1]
+    k = min([i-1,m-i,j-1,n-j])
 
-    S = dct(dct(a1, axis=0), axis=1) / dct(dct(e1,axis=0), axis=1)
-    return np.max(S), S
+    PP = psf[i-(k+1):i+(k),j-(k+1):j+(k)]
+    Z1 = torch.diag(torch.ones(k+1), k)
+    Z2 = torch.diag(torch.ones(k), k+1)
+
+    PP = torch.matmul(torch.matmul(Z1,PP),Z1.T) + torch.matmul(torch.matmul(Z1,PP),Z2.T) \
+        + torch.matmul(torch.matmul(Z2,PP),Z1.T) + torch.matmul(torch.matmul(Z2,PP),Z2.T)
+    Ps = torch.zeros(m,n)
+    Ps[0:2*k+1,0:2*k+1] = PP
+
+    return Ps
+
+def evals_blur(shape=(9,9),std=4,ret_torch=False):
+    """Calculates eigenvalues according to equation in Deblurring Images"""
+    if ret_torch == False:
+        psf = fspecial(shape,std)
+        a1 = dctshift(psf)
+        
+        e1 = np.zeros_like(a1)
+        e1[0,0] = 1
+
+        S = dct(dct(a1, axis=0), axis=1) / dct(dct(e1,axis=0), axis=1)
+        return np.max(S), S
+    if ret_torch:
+        psf = fspecial(shape,std,True)
+        a1 = dctshift_torch(psf)
+
+        e1 = torch.zeros_like(a1)
+        e1[0,0] = 1.0
+
+        S = dct_t.dct_2d(a1) / dct_t.dct_2d(e1) # https://github.com/zh217/torch-dct/tree/master
+        return torch.max(S), S
 
 def gen_function(x,b):
     Ax = blur_operator_torch(x)
